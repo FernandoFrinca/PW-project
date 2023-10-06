4ee7dd0225e1f9124d23dae24b441175c6df0bf1
Tomus Nicolas <nicolas.tomus@student.upt.ro>
2023-10-06 14:25:19 +0300

    Login with Firebase

--- cinemavillage/lib/screens/home_screen.dart
+++ cinemavillage/lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cinemavillage/screens/signIn_screen.dart';
 
@@ -16,8 +18,12 @@ class _HomeScreenState extends State<HomeScreen> {
         child: ElevatedButton(
           child: Text("Logout"),
           onPressed: () {
-              Navigator.push(context,
+              FirebaseAuth.instance.signOut().then((value) {
+                  print("Signed Out");
+                  Navigator.push(context,
                   MaterialPageRoute(builder: (context) => SignInScreen())); 
+              });
+              
           },
         ),
       ),
--- cinemavillage/lib/screens/signIn_screen.dart
+++ cinemavillage/lib/screens/signIn_screen.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cinemavillage/reusable_widgets/reusable_widget.dart';
 import 'package:cinemavillage/screens/home_screen.dart';
@@ -31,7 +33,7 @@ class _SignInScreenState extends State<SignInScreen> {
             child: Column(
               children: <Widget>[
                 logoWidget("assets/images/Cinema_village_2.png"),
-                reusableTextField("Your username", Icons.person_outline, false, _emailTextController),
+                reusableTextField("Your email", Icons.person_outline, false, _emailTextController),
                 SizedBox(
                   height: 20,
                 ),
@@ -40,8 +42,15 @@ class _SignInScreenState extends State<SignInScreen> {
                   height: 20,
                 ),
                 singInSingUpButton(context, true, () {
-                  Navigator.push(context, 
-                  MaterialPageRoute(builder: (context) => HomeScreen()));
+                  FirebaseAuth.instance.signInWithEmailAndPassword(
+                    email: _emailTextController.text, 
+                    password: _passwordTextController.text).then((value) {
+                        Navigator.push(context, 
+                        MaterialPageRoute(builder: (context) => HomeScreen()));
+                    }).onError((error, stackTrace) {
+                      print("Error ${error.toString()}");
+                    });
+                  
                 }),
                 signUpOption(),
                 
