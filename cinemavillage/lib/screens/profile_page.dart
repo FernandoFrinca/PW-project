19679414dc0bfd9a68055395e4d6183af802627f
TomusNicolas <117032443+TomusNicolas@users.noreply.github.com>
2023-10-27 20:38:22 +0300

    Revert "API"

--- cinemavillage/lib/screens/profile_page.dart
+++ cinemavillage/lib/screens/profile_page.dart
@@ -1,5 +1,9 @@
 import 'package:cinemavillage/reusable_widgets/reusable_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cinemavillage/screens/signIn_screen.dart';
+import 'package:cinemavillage/screens/edit_page.dart';
 
 class Profile_screen extends StatefulWidget {
   const Profile_screen({super.key});
@@ -9,12 +13,176 @@ class Profile_screen extends StatefulWidget {
 }
 
 class _Profile_screenState extends State<Profile_screen> {
+  String? name = '';
+  String? email = '';
+  String? gender = '';
+  Timestamp? date_time = null;
+  DateTime? birthday = null;
+  String? image = '';
+
+  Future _getDataFromDatabase() async {
+    await FirebaseFirestore.instance
+        .collection("users")
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get()
+        .then((snapshot) async {
+      if (snapshot.exists) {
+        setState(() {
+          name = snapshot.data()!["username"];
+          email = snapshot.data()!["email"];
+          gender = snapshot.data()!["gender"];
+          date_time = snapshot.data()!["birthday"];
+          birthday = date_time?.toDate();
+          image = snapshot.data()!["userImage"];
+        });
+      }
+    });
+  }
+
   @override
+  void initState() {
+    super.initState();
+    _getDataFromDatabase();
+  }
+
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height;
-    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Top_bar(screenHeight, screenWidth),
+      body: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height - 66,
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 241, 81, 37),
+              Color.fromARGB(255, 25, 25, 25)
+            ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(20, 40, 20, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 100,
+                          height: 100,
+                          child:
+                              Image.asset("assets/images/Cinema_village_2.png"),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Stack(
+                          children: [
+                            CircleAvatar(
+                              radius: 70,
+                              backgroundColor: Colors.black54,
+                              child: CircleAvatar(
+                                radius: 64,
+                                backgroundImage: NetworkImage(image!),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        printText(name!, Icons.person_outline, 335, 50),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        printText(email!, Icons.email_outlined, 335, 50),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        printText(
+                            '${birthday?.day}/${birthday?.month}/${birthday?.year}',
+                            Icons.calendar_month_outlined,
+                            335,
+                            50),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        printText(
+                            gender!, Icons.people_outline_outlined, 335, 50)
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 80,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              minimumSize: Size(130, 40)),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Edit_screen()));
+                          },
+                          child: Text('Edit Profile'),
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        ElevatedButton(
+                          child: Text("Logout"),
+                          style: ElevatedButton.styleFrom(
+                              minimumSize: Size(130, 40)),
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SignInScreen()));
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
